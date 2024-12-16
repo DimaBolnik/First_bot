73ce2360d51098607b56335090573d8fcc76a91a
User <dimabolnik@mail.ru>
2024-12-16 17:31:59 +0300

    First_bot(0.1)

---
+++ dispatcher/src/main/java/ru/bolnik/dima/controller/UpdateController.java
@@ -0,0 +1,88 @@
+package ru.bolnik.dima.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.bolnik.dima.services.UpdateProducer;
+import ru.bolnik.dima.utils.MessageUtils;
+
+import static ru.bolnik.dima.model.RabbitQueue.*;
+
+@Component
+@Log4j
+public class UpdateController {
+
+    private TelegramBot telegramBot;
+
+    private final MessageUtils messageUtils;
+
+    private final UpdateProducer updateProducer;
+
+    public UpdateController(MessageUtils messageUtils, UpdateProducer updateProducer) {
+        this.messageUtils = messageUtils;
+        this.updateProducer = updateProducer;
+    }
+
+    public void registerBot(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update) {
+        if (update == null) {
+            log.error("Update is null");
+            return;
+        }
+
+        if (update.getMessage() != null) {
+            distributeMessagesByType(update);
+        }else {
+            log.error("Received unsupported message type " + update);
+        }
+    }
+
+    private void distributeMessagesByType(Update update) {
+        Message message = update.getMessage();
+        if (message.getText() != null) {
+            processTextMessage(update);
+        } else if (message.getDocument() != null) {
+            processDocMessage(update);
+        } else if (message.getPhoto() != null) {
+            processPhotoMessage(update);
+        } else {
+            setUnsupportedMessageTypeView(update);
+        }
+    }
+
+    private void setUnsupportedMessageTypeView(Update update) {
+        SendMessage sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Неподдерживаемый тип сообщений");
+        setView(sendMessage);
+    }
+
+    private void setFileIsReceivedView(Update update) {
+        SendMessage sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Файл получен! Обрабатывается...");
+        setView(sendMessage);
+    }
+
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+
+
+    private void processPhotoMessage(Update update) {
+        updateProducer.produce(PHOTO_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processDocMessage(Update update) {
+        updateProducer.produce(DOC_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processTextMessage(Update update) {
+        updateProducer.produce(TEXT_MESSAGE_UPDATE, update);
+    }
+}
---
+++ dispatcher/src/main/java/ru/bolnik/dima/utils/MessageUtils.java
@@ -0,0 +1,17 @@
+package ru.bolnik.dima.utils;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class MessageUtils {
+    public static SendMessage generateSendMessageWithText(Update update, String text) {
+        Message message = update.getMessage();
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText(text);
+        return sendMessage;
+    }
+}
